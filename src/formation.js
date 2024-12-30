62922053876bc7310bcc92827b6433bfe1a5519c
VitorHSilver <vitor-henrique1996@hotmail.com>
2024-12-30 18:19:54 -0300

    Refactor styles in Formation component for improved layout and responsiveness

--- src/formation.js
+++ src/formation.js
@@ -18,7 +18,7 @@ function Formation() {
                          </p>
                     </div>
                     <div className="flex justify-around mt-24 max-lg:gap-1 max-sm:gap-2  max-smartphone:gap-1 max-smartphone:grid">
-                         <div className="grid bg-bgBlack p-3 text-center border-crocoGreen max-md:border max-sm:border rounded-lg">
+                         <div className="grid gap-y-4 bg-bgBlack rounded-lg p-3 text-center max-sm:border border-gray-50 max-md:border max-sm:shadow-none    shadow-sm shadow-slate-50/50  hover:shadow-emerald-300  transition duration-1000 ease-in">
                               <h4 className="uppercase text-center text-gray-100/25">Tecnólogo</h4>
                               <p className="text-gray-100">Análise e desenvolvimento de sistemas</p>
                               <p className="text-gray-100/50">
@@ -26,7 +26,7 @@ function Formation() {
                               </p>
                          </div>
 
-                         <div className="grid gap-y-4 bg-bgBlack rounded-lg p-3 text-center max-sm:border border-crocoGreen max-md:border max-sm:shadow-lg shadow-gray-100/50 max-smartphone:mt-4">
+                         <div className="grid gap-y-4 bg-bgBlack rounded-lg p-3 text-center max-sm:border border-gray-50 max-md:border max-sm:shadow-none  max-smartphone:mt-4 shadow-sm shadow-slate-50/50  hover:shadow-emerald-300  transition duration-1000 ease-in">
                               <h4 className="uppercase text-center text-gray-100/25">Técnico</h4>
                               <p className="text-gray-100">Técnico em informática</p>
                               <p className="text-gray-100/50">
@@ -35,7 +35,7 @@ function Formation() {
                          </div>
                     </div>
                     <div className="mt-24">
-                         <h3 className="text-gray-100/50 text-3xl font-semibold text-center max-sm:mb-10 ">Cursos</h3>
+                         <h3 className="text-gray-100/50 text-3xl font-semibold text-center max-sm:mb-10  ">Cursos</h3>
                          <div className="flex flex-col items-center mt-8 text-center">
                               <div className="grid grid-cols-2 gap-4 w-3/4">
                                    <p className="text-gray-100">UX Design & UI Design</p>
